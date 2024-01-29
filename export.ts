db7a948a5e158cb3a1b8b0d8c9c426cf7194c826
Benjie Gillam <benjie@jemjie.com>
2024-01-29 10:35:41 +0000

    Copy of setup in issue

---
+++ export.ts
@@ -0,0 +1,21 @@
+import { makeSchema } from "postgraphile";
+import { exportSchema } from "graphile-export";
+import preset from "../src/graphile.config.js";
+import path from "path";
+
+const __dirname = path.resolve();
+
+const main = async () => {
+  console.log(
+    `Exporting a GraphQL schema from the database at ${preset?.pgServices?.[0].name}...`,
+  );
+
+  const { schema, resolvedPreset } = await makeSchema(preset);
+  const exportFileLocation = `${__dirname}/src/exported-schema.js`;
+
+  await exportSchema(schema, exportFileLocation, {
+    mode: "typeDefs",
+  });
+};
+
+await main();
