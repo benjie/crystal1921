1ba09f8ce2fe8a6b028dd21354f9ad7b68795604
Benjie Gillam <benjie@jemjie.com>
2024-01-29 10:55:05 +0000

    Replace loads of envvars with one

--- graphile.config.ts
+++ graphile.config.ts
@@ -2,23 +2,7 @@
 import { makePgService } from "postgraphile/adaptors/pg";
 import { PostGraphileAmberPreset } from "postgraphile/presets/amber";
 
-interface PgConnectConfig {
-  user: string;
-  database: string;
-  password: string;
-  host: string;
-  port: string;
-}
-
-const pgConfig: PgConnectConfig = {
-  user: process.env.TSDB_USERNAME! || "postgres",
-  password: process.env.TSDB_PASSWORD! || "password",
-  database: process.env.TSDB_DATABASE! || "local_test",
-  host: process.env.TSDB_HOST! || "127.0.0.1",
-  port: process.env.TSDB_PORT! || "5432",
-};
-
-const connectionString = `postgres://${pgConfig.user}:${pgConfig.password}@${pgConfig.host}:${pgConfig.port}/${pgConfig.database}`;
+const connectionString = process.env.TSDB_CONNECTION_STRING;
 
 const preset: GraphileConfig.Preset = {
   extends: [PostGraphileAmberPreset],
