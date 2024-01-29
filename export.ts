c54c0f9cac051d37580efeae50a1c16c6511806c
Benjie Gillam <benjie@jemjie.com>
2024-01-29 10:59:24 +0000

    Simplify and release DB when done

--- export.ts
+++ export.ts
@@ -1,21 +1,19 @@
 import { makeSchema } from "postgraphile";
 import { exportSchema } from "graphile-export";
-import preset from "./graphile.config.js";
+import preset, { pgService } from "./graphile.config.js";
 import path from "path";
 
 const __dirname = path.resolve();
 
-const main = async () => {
-  console.log(
-    `Exporting a GraphQL schema from the database at ${preset?.pgServices?.[0].name}...`,
-  );
+console.log(
+  `Exporting a GraphQL schema from the database at ${preset?.pgServices?.[0].name}...`,
+);
 
-  const { schema, resolvedPreset } = await makeSchema(preset);
-  const exportFileLocation = `${__dirname}/exported-schema.js`;
+const { schema, resolvedPreset } = await makeSchema(preset);
+const exportFileLocation = `${__dirname}/exported-schema.js`;
 
-  await exportSchema(schema, exportFileLocation, {
-    mode: "typeDefs",
-  });
-};
+await exportSchema(schema, exportFileLocation, {
+  mode: "typeDefs",
+});
 
-await main();
+await pgService.release?.();
--- graphile.config.ts
+++ graphile.config.ts
@@ -4,9 +4,11 @@ import { PostGraphileAmberPreset } from "postgraphile/presets/amber";
 
 const connectionString = process.env.TSDB_CONNECTION_STRING;
 
+export const pgService = makePgService({ connectionString });
+
 const preset: GraphileConfig.Preset = {
   extends: [PostGraphileAmberPreset],
-  pgServices: [makePgService({ connectionString })],
+  pgServices: [pgService],
 };
 
 export default preset;
